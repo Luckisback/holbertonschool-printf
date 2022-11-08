9ce9156070df866952f0db10fcb64392f5110b50
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-08 04:25:41 -0800

    corrected main.c

--- _main.c
+++ _main.c
@@ -7,12 +7,7 @@
  */
 int main(void)
 {
-	int len;
-	int len2;
-
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
+	return (0);
 }
