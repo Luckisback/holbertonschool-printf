c68d2965229cb97e5ce36002a37d699ee973127e
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-08 01:09:01 -0800

    changes to main.h, print_char, print_percent and print_string functions

--- main.h
+++ main.h
@@ -5,13 +5,21 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef struct print {
+/**
+ * struct print - the structure of printf function
+ * Description: associat each data type to the function
+ * @data: the type of data
+ * @f: the funtion pointer to the printing functions used
+ **/
+typedef struct print
+{
 	char *data;
 	void (*f)(va_list arg);
 } print_t;
 
 int _printf(const char *format, ...);
-void print_string(va_list arg);
+void print_string(va_list arg, char *str);
 void print_char(va_list arg);
+void print_percent(va_list arg);
 
 #endif
