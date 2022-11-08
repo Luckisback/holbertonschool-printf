1aa9de2fb06fffa3de84c918d6b1d0e0c302e6e4
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-08 04:31:59 -0800

    corrected compilation errors from print string, print char, putchar

--- print_char.c
+++ print_char.c
@@ -9,5 +9,5 @@ void print_char(va_list arg)
 {
 	char c;
 	c = va_arg(arg, int);
-	write(1, c, 1);/* affichage du caractere reçu */
+	_putchar(c);/* affichage du caractere reçu */
 }
--- print_string.c
+++ print_string.c
@@ -11,6 +11,7 @@ void print_string(va_list arg)
 {
 	int length = 0;
 	int i = 0;
+	char *str;
 
 	str = va_arg(arg, char*);
 	for (i = 0; str[i]; i++)
